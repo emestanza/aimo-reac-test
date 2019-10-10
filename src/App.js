e2a9feff22133baaac686f64ddf1eb88a54d9b5e
Enmanuel Mestanza <emestanza@vivienda.gob.pe>
2019-10-10 12:48:55 -0500

    Adding search username form and axios request

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
+import Form from './components/Form';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  // Método para consultar la API de Letras de canciones
+  const consumeApiGithub = async search => {
+    console.log(search);
+    const {username} = search;
+    const url = `https://api.github.com/users/${username}/repos`;
+
+    // consultar la api
+    const resultado = await axios.get(url,{
+      headers: { Authorization: "token a527fbf293ab502984a087a7090a233f7327fcbe" }
+  });
+
+    console.log("DATA");
+    console.log(resultado);
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Form consumeApiGithub ={consumeApiGithub} />
+    </Fragment>
   );
+
 }
 
 export default App;
