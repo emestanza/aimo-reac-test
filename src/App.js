94c14a4793c6471b9eccfe18528e4f2fbe92affa
Enmanuel Mestanza <emestanza@vivienda.gob.pe>
2019-10-10 13:59:01 -0500

    Dotenv config file to set github access token

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Form from './components/Form';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
+
+const tokenStr = `token ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`;
 
 function App() {
-  
+
   // Método para consultar la API de Letras de canciones
   const consumeApiGithub = async search => {
-    console.log(search);
+
     const {username} = search;
     const url = `https://api.github.com/users/${username}/repos`;
 
     // consultar la api
     const resultado = await axios.get(url,{
-      headers: { Authorization: "token a527fbf293ab502984a087a7090a233f7327fcbe" }
+      headers: { Authorization: tokenStr}
   });
 
     console.log("DATA");
